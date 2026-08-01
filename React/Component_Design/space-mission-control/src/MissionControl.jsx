7b7e9a9aba01dd3512e7feaaf8f4668869f46ae2
unknown <18756859+brandonholderman@users.noreply.github.com>
2026-07-31 20:38:04 -0700

    saving

--- React/Component_Design/space-mission-control/src/MissionControl.jsx
+++ React/Component_Design/space-mission-control/src/MissionControl.jsx
@@ -5,26 +5,27 @@ import './MissionControl.css'
 
 function MissionControl({ missions }) {
 
+    // each time a filter button is clicked, we will update state missionList to on missions that match that category. 
     const [missionList, setMissionList] = useState(missions);
-    const [missionStatus, setMissionStatus] = useState(null);
+    // const [missionStatus, setMissionStatus] = useState(null);
 
-    const filterItem = (curcat) => {
-        const newItem = Data.filter((newVal) => {
-            return newVal.category === curcat;
-            // comparing category for displaying data
-        });
-        setItem(newItem);
-    };
+    // const filterItem = (curcat) => {
+    //     const newItem = Data.filter((newVal) => {
+    //         return newVal.category === curcat;
+    //         // comparing category for displaying data
+    //     });
+    //     setItem(newItem);
+    // };
 
-    function filterMission(selectedStatus) {
-        if (selectedStatus === )
-    }
+    // function filterMission(selectedStatus) {
+    //     if (selectedStatus === )
+    // }
 
 
     return (
         <div>
-            {/* <MissionFilter status={missionList.map(mission => mission.status)}/> */}
-            <MissionFilter status={missionStatus} onClick={() => filterMission()} />
+            <MissionFilter status={missionList.map(mission => mission.status)}/>
+            {/* <MissionFilter status={missionStatus} onClick={() => filterMission()} /> */}
             {missionList.map(mission => (
                 <MissionCard key={mission.id} id={mission.id} name={mission.name} status={mission.status} crew={mission.crew}/>
             ))}
